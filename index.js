d406ec88f792be8f8b12aa1b0c34f41e3cc5badd
Giulliano Antonini <giullianoantonini@gmail.com>
2024-04-28 13:00:16 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,24 @@
+require("./index.css")
+
+console.log("Olá WebPack");
+
+document.getElementById("botaoConcatenar").addEventListener("click", executarConcatenar)
+document.getElementById("botaoSomar").addEventListener("click", executarDividir)
+
+function executarConcatenar() {
+    console.log("Fui clicado")
+
+    const valor1 = (document.getElementById("caractereUm").value)
+    const valor2 = (document.getElementById("caractereDois").value)
+    const resultado = valor1 + " " + valor2
+    document.getElementById("resultadoRetornado").innerText = resultado
+    return resultado
+}
+
+function executarDividir() {
+    const num1 = Number(document.getElementById("numeroUm").value)
+    const num2 = Number(document.getElementById("numeroDois").value)
+    const resultado = num1 / num2
+    document.getElementById("resultadoDividir").innerText = resultado
+    return resultado
+}
